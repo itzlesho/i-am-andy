ebb621c4b99ede76c429b01ca9cf388810510764
itzlesho <102629685+itzlesho@users.noreply.github.com>
2023-04-03 15:55:09 -0700

    Create andy.cpp

---
+++ andy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int LinearSearch(int[], int, int);
+
+int main() {
+        int numbers[] = { 2, -5, 14, -29 };
+        int value;
+    int* p = new int[10];
+        cout << "Enter a number to search for: ";
+        cin >> value;
+        cout << "Index found: " << LinearSearch(numbers, sizeof(numbers) / sizeof(numbers[0]), value) << endl;
+         delete[] p;
+        return 0;
+}
+
+int LinearSearch(int array[], int length, int desiredValue) {
+        int i;
+
+        for (i = 0; i <= length; i++) {   //BUG: the test i<=length is wrong
+                if (desiredValue == array[i]) {
+                        break;
+                }
+        }
+        return i;
+}
